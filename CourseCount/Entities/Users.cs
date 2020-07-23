1ce81f16dab11d5df22d6557545e3c1c97b86744
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-22 21:45:53 -0300

    My first GetHashcode

---
+++ CourseCount/Entities/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CourseCount.Entities
+{
+    class Users
+    {
+        public int Id { get; set; }
+        
+
+        public Users()
+        {
+
+        }
+
+        public Users(int id)
+        {
+            Id = id;
+           
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Users))
+            {
+                return false;
+            }
+            Users other = obj as Users;
+            return Id.Equals(other.Id);
+        }
+    }
+    
+}
---
+++ CourseCount/Program.cs
@@ -0,0 +1,39 @@
+﻿using CourseCount.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CourseCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Users> users = new HashSet<Users>();
+
+            Console.Write("How many Users for Course A: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                users.Add(new Users {Id=id });
+            }
+            Console.Write("How many Users for Course B: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                users.Add(new Users { Id = id });
+            }
+            Console.Write("How many Users for Course C: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                users.Add(new Users { Id = id });
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(users.Count);
+        }
+    }
+}
